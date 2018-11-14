5187dc5e65dfc21e1c3b2c13d7745bf85b792e38
João Lucas <joaolucaspachecoab@gmail.com>
2018-11-14 12:45:37 -0200

    Main function added

--- bissexto/src/main/kotlin/github/jlucas5/kotlin/LeapYear.kt
+++ bissexto/src/main/kotlin/github/jlucas5/kotlin/LeapYear.kt
@@ -2,11 +2,18 @@ package github.jlucas5.kotlin
 
 fun main(args: Array<String>) {
 
+    println("Qual ano você deseja verificar?")
+    val input = readLine()
+    val calculator = LeapYearIdentifier()
+    if (calculator.isLeapYear(input)){
+        println("O ano $input é bissexto")
+    }else println("O ano $input não é bissexto")
+
 
 }
 class LeapYearIdentifier{
     fun isLeapYear(year:Int) {
-        return if((year % 4 === 0 && year % 100 === 0) || (year % 400 === 0){
+        return if((year % 4 == 0 && year % 100 == 0) || (year % 400 == 0){
             true
         }else false
 }
